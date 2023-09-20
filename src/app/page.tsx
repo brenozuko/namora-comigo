8c79fdb98503f41654ea0284d4b23fbcdb476333
Breno Zukowski <brenozukotech@gmail.com>
2023-09-20 11:19:04 -0300

    Remove audio from first page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { Container } from "@/styles/containers";
 import { PressStart } from "@/styles/home";
 
@@ -7,8 +8,6 @@ export default function Home() {
       <a style={{ textDecoration: "none" }} href="/pergunta-pra-leticia">
         <PressStart>APERTE START</PressStart>
       </a>
-
-      <audio src="/hp.mp3" autoPlay={true} loop={true} />
     </Container>
   );
 }
